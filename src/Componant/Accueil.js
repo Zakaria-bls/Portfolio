7faaacd775e5fa7282b43c94f00b3de29e1ef81b
alizhassane <hassanealiz11@gmail.com>
2023-11-25 20:32:54 +0100

    first commit

---
+++ src/Componant/Accueil.js
@@ -0,0 +1,157 @@
+import React, { useRef } from 'react';
+
+// import CSS
+import "../componantStyle/accueil.css"
+import "../componantStyle/header.css"
+import "../componantStyle/footer.css"
+
+
+// import componant
+import Home from './Home';
+import About from './About';
+import Work from './Work';
+import Contact from './Contact';
+
+//import images
+import Facebook from '../assets/Facebook.svg'
+import instagram from '../assets/Instagram.svg'
+import linkedin from '../assets/LinkedIn.svg'
+import git from '../assets/git.svg'
+import arrowMail from '../assets/arrowEmail.svg'
+
+import ZB from './ZB'
+import { useState } from 'react';
+
+
+
+const Accueil = () => {
+    const sectionRefs = {
+        header: useRef(null),
+        section1: useRef(null),
+        section2: useRef(null),
+        section3: useRef(null),
+        section4: useRef(null),
+    };
+
+    const [skill, setSkill] = useState(true)
+
+    const scrollToSection = (section) => {
+        sectionRefs[section].current.scrollIntoView({
+            behavior: 'smooth',
+        });
+    };
+
+       
+    return (
+    <div className='Container' ref={sectionRefs.header}>
+        <div className='headerContainer'>
+            <div className='insideHeaderContainer'>
+                <div id='logoContainer'>
+                    <p className='logo'>Z.B</p>
+                </div>
+                <ul className='headerList'>
+                    <li className='listHeaderElement' onClick={() => scrollToSection('section1')}>
+                        <p>Home</p>
+                    </li>
+                    <li className='listHeaderElement' onClick={() => {scrollToSection('section2'); setSkill(true)}}>
+                        <p>About</p>
+                    </li>
+                    <li className='listHeaderElement' onClick={() => {scrollToSection('section2'); setSkill(false)}}>
+                        <p>Skills</p>
+                    </li>
+                    <li className='listHeaderElement' onClick={() => scrollToSection('section3')}>
+                        <p>Portfolio</p>
+                    </li>
+                    <li className='listHeaderElement' onClick={() => scrollToSection('section4')}>
+                        <p>Contact</p>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        
+            <div className='home' ref={sectionRefs.section1}>
+                 <Home scrollToSection={scrollToSection} section="section2"/> 
+            </div>
+            <div className='about' ref={sectionRefs.section2}>
+                 <About skill={skill} setSkill={setSkill} scrollToSection={scrollToSection} section="section3"/>
+                 <ZB  top="35.5vw" right="60vw"/>
+            </div>
+            <div className='work' ref={sectionRefs.section3}>
+                <Work scrollToSection={scrollToSection} section="section4"/>
+            </div>
+            <div ref={sectionRefs.section4}>
+                 <Contact/>
+            </div>
+
+            <div className='footerContainer'>
+        <div className='footer'>
+            <div className='footerSection s1'>
+            
+            <div className='s1Up'>
+                <p className='logo' onClick={() => scrollToSection('header')}>Z.B</p>
+                <div className='s1Txt Txt'>
+                
+                <p>Crafting digital experiences with passion and precision.</p>
+                <p>Turning ideas into innovative web and desktop solutions.</p>
+                <p>Dedicated to delivering excellence in web and app development.</p>
+                <p>Let's collaborate on your next digital journey.</p>
+                </div>
+
+                <div className='s1Down'>
+                    <div className='socialMedia'>
+                        <a href='https://github.com/Zakaria-bls' target='_blank'  rel="noopener noreferrer"><img src={git} alt=''/></a>
+                        <a href='https://www.linkedin.com' target='_blank'  rel="noopener noreferrer"><img src={linkedin} alt=''/></a>
+                        <a href='https://www.instagram.com' target='_blank'  rel="noopener noreferrer"><img src={instagram} alt=''/></a>
+                        <a href='https://www.facebook.com' target='_blank'  rel="noopener noreferrer"><img src={Facebook} alt=''/></a>
+                    </div>
+
+                    <div className='Txt'>
+                    <p>© 2023 . All rights reserved.</p>
+                    </div>
+                </div>
+
+            </div>
+
+            </div>
+
+            <div className='footerSection footerSame s2'>
+            <p className='footerSameTitle'>Take a tour</p>
+            <div className='footerDiv'>
+                <p to="/Partner" className='footerSameTxt'>Partner</p>
+                <p className='footerSameTxt'>Products</p>
+            </div>
+            </div>
+
+            <div className='footerSection footerSame s3'>
+            <p className='footerSameTitle'>Our Company</p>
+            <div className='footerDiv'>
+                <p className='footerSameTxt'>à propos de nous</p>
+                <a className='footerSameTxt' href='https://goo.gl/maps/RJdasJh5WxXd7BBv9' target='_blank'  rel="noopener noreferrer">Localisation</a>
+                <p className='footerSameTxt'>Contacter nous</p>
+            </div>
+            </div>
+
+            <div className='footerSection footerSame s4'>
+            <p className='footerSameTitle'>Subscribe</p>
+            
+            <div className='Txt inputTxt'>
+                <p>Abonnez-vous pour recevoir les derniers produits et nouvelles de nous</p>
+                
+                <div className='inputContainer'>
+                <input type='email' placeholder='Email Address'/>
+                <div className='sendMail'>
+                    <img src={arrowMail} alt=''/>
+                </div>
+                </div>
+            </div>
+            </div>
+
+        </div>
+</div>
+    </div>
+
+    
+    );
+};
+
+export default Accueil;
---
+++ src/Componant/Home.js
@@ -0,0 +1,59 @@
+import React from 'react'
+
+// import css
+import '../componantStyle/home.css'
+
+//import assets
+import download from '../assets/Download.svg'
+import profile from '../assets/profilePic.svg'
+import Cv from '../assets/CVZakaria.pdf'
+
+//import componant
+import SchrollDown from './SchrollDown'
+import ZB from './ZB'
+
+export default function Home({ scrollToSection, section }) {
+
+  return (
+    <div className='homeContainer'>
+      <div className='insideHomeContainer'>
+          <div className='insideHome'>
+            <div className='insideHomeText'> 
+                <div className='homeTextGras'>
+                    <p id='name'>Hi, I’m Zakaria BOULSANE </p>
+                    <p id='work'>Junior Developer</p>
+                </div>
+                <div className='txtDownlaod'>
+                    <div className='homeTextRegular'>
+                        <p>As a computer science master's degree holder with expertise in software engineering and data processing, I have gained experience in full-stack web and software development. </p>
+                    </div>
+                    <div className='homeButton'>
+                      <a href={Cv} download='CV-Zakaria-BLS'>
+                          <p>Download CV</p>
+                          <img src={download} alt=''/>
+                      </a>
+                    </div>
+                </div>
+            </div>
+            <div className='insideHomePic'>
+                <div id='cercileOfImage'>
+                  <img src={profile} alt=''/>
+                </div>
+            </div>
+          </div>
+
+          <div className='darkMode'>
+            <p id='light'>Light</p>
+            <p id='slach'>/</p>
+            <p id='dark'>Dark</p>
+          </div>
+      </div>
+
+      
+      <ZB top="-7.5vw" right="-15.5vw"/>
+
+      <SchrollDown bottom="5vw" scrollToSection={scrollToSection} section = {section}/>
+
+    </div>
+  )
+}
